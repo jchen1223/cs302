8ac94559bc1cff452d4bd1b7c11247e2f071c058
Jeffrey Johnson Chen <jchen122@tesla10.eecs.utk.edu>
2024-09-11 14:20:20 -0400

    changes to list.cpp (unsure of dump node), stl.cpp (finished, but may need to check))

--- proj2/list.cpp
+++ proj2/list.cpp
@@ -14,3 +14,34 @@ List::~List() {
   // node that the ptr pointer points to. since you deleted it, you cannot
   // access the next node.
 }
+
+bool node_number_compare(const Node *a, const Node *b) {
+  int aNum = a->number;
+  int bNum = b->number;
+
+  // allows for it to be sorted in ascending order.
+  if (aNum < bNum) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool node_string_compare(const Node *a, const Node *b) {
+  std::string aNum = a->string;
+  std::string bNum = b->string;
+  if (aNum < bNum) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Prints out the node passed in and the other nodes connected and onwards
+void dump_node(Node *n) {
+  while (n->next != NULL) {
+    std::cout << n->number << std::endl;
+    std::cout << n->next << std::endl;
+    n->next = n;
+  }
+}
--- proj2/stl.cpp
+++ proj2/stl.cpp
@@ -4,9 +4,49 @@
 
 #include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 void stl_sort(List &l, bool numeric) {
-  vector<int, List> vec;
-  if (numeric == true) {
+  // Create a vector that will hold all the pointers to each of the indiviudal
+  // nodes in the list.
+  vector<Node *> vec;
+
+  // Fill the vector with all the node *;
+  // Node* curr is outside because if it was in the for loop it would always set
+  // it to beginning. Node* curr is an independent pointer of head pointer, so
+  // updating it does not change head pointer. Node* curr access head pointer
+  // because current is set to point to the same node as head.
+  Node *curr = l.head;
+  while (curr != NULL) {
+    vec.push_back(curr);
+    curr = curr->next;
   }
+
+  // Now we want to call sort on the vector
+
+  // Function pointers, sort need a way to compare so we pass in the function
+  // pointer to compare. When you use the function name without (), you're
+  // passing a pointer to the function.
+  if (numeric == true) {
+    sort(vec.begin(), vec.end(), node_number_compare);
+  } else {
+    sort(vec.begin(), vec.end(), node_string_compare);
+  }
+
+  // Now we want to relink the nodes in the list
+  // This will relink every node beside the one the head pointer is pointing to.
+  for (size_t i = 0; i < l.size - 1; i++) {
+    // We want to save the old list so we can properly relink each node, so we
+    // save it on a tempNode before updating the old list.
+    vec[i]->next = vec[i + 1];
+  }
+  // This will fix the last node to point to NULL
+  vec[l.size - 1]->next = NULL;
+
+  // Move the head pointer to the newly sorted list made from the vector. This
+  // way it will print out the newly sorted linked list.
+  l.head = vec[0];
 }
