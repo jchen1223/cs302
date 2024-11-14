33e8f37369a2551a2f156c9ff1fec7f5b626b132
Jeffrey Johnson Chen <jchen122@tesla10.eecs.utk.edu>
2024-11-13 22:08:56 -0500

    packaging submission

--- proj5/README.md
+++ proj5/README.md
@@ -1,1 +1,24 @@
+### Name: Jeffrey Chen and Aidan Feyerherm
+
+### Contributions:
+- Jeffrey Chen:
+- Wrote adjust()
+- Wrote ~Graph(); the destructor
+- Reviewed the code written
+- Wrote README.md and the report
+
+- Aidan Feyerherm:
+- Set up the classes
+- Wrote main()
+- Wrote Graph(); the constructor
+- Wrote makepath(); the BFS
+- Wrote reset()
+- Wrote findpath()
+
+### Questionaire:
+- What is the complexity?
+- The time complexity is O(W * (V + E) * V^3) which can be simplified to O(W * V^4).
+- The W is the number of Words, the V is the number of vertices/nodes, and the E is the number of edges.
+- For each word, we run the program, so that includes W, then for each word we need to constantly call BFS which is O(V+E), then after we call BFS we need to use the makepath() and adjust() functions which both are O(V), however, toogether they are O(V^2). Then we have one final for loop to reset the backedges which another O(V) so thus it would be O(V^3) once we account everything. Then we reset the loop with another BFS iteration, however, that is already included in the beginning and thus our implementation is O(W * (V+E) * V^3). However, we can simplify this to O(W * V^4).
+
 
--- proj5/worddice.cpp
+++ proj5/worddice.cpp
@@ -1,234 +1,300 @@
 // Project 5 - Word Dice
+// Name: Aidan Feyerherm and Jeffrey Chen
 // Takes an input of dice with letters, checks a list of
-// words to see if the dice can make those words
+// words to see if the dice can make those words using network flow
+// and Edmond Karp's algorithm.
 // Last updated: 11/7/2024
 
-#include <iostream>
-#include <cstdlib>
 #include <cstdio>
+#include <cstdlib>
+#include <deque>
+#include <fstream>
+#include <iostream>
 #include <map>
 #include <set>
-#include <vector>
 #include <string>
-#include <fstream>
-#include <deque>
+#include <vector>
 using namespace std;
 
 class Node {
 public:
-	int id;
-	vector <int> booleans;
-	set <int> adj;
-	Node *backedge;
-	int dist;
-
+  int id;
+  vector<int> booleans;
+  set<int> adj;   // Using set so there are no duplicate edges.
+  Node *backedge; // Backwards edge; the node we "came" from.
+  int dist;       // Essentially the original
 };
 
-// A double ended queue is used for Edmonds-Karp algorithm to find the augmenting path 
+// A double ended queue is used for Edmonds-Karp algorithm to find the
+// augmenting path
 class Graph {
 public:
-	Graph(vector <string>);
-~Graph();
-	deque <int> q;
-	vector <pair<int,int> > path;
-	vector <Node *> nodes;
-	void reset();
-	void makegraph(string);
-	void findpath();
-	void adjust();
-	void  makepath();
-	vector <string> dice;
-
+  Graph(vector<string>);
+  ~Graph();
+  deque<int> q;
+  vector<pair<int, int>> path; // Stores the augmenting path found by BFS
+  vector<Node *> nodes;
+  void reset();
+  void makegraph(string);
+  void findpath();
+  void adjust();
+  void makepath();
+  vector<string> dice;
 };
 
-// Sets the dice vector equal to the given vector 
-Graph::Graph(vector <string> s1) {
-	int i, j;
-	Node *n;
+// Sets the dice vector equal to the given vector
+Graph::Graph(vector<string> s1) {
+  int i, j;
+  Node *n;
 
-	dice = s1;
-	nodes.resize((2*dice.size())+2);
+  dice = s1;
 
-	// Initialize all nodes
-	for (i = 0; i < nodes.size(); i++) {
-		n = new Node;
-		if (i == 0 || i == nodes.size()-1) 
-			n->booleans.resize(256, 1);
-		else
-			n->booleans.resize(256, 0);
-		n->id = i;
-		n->dist = -1;
-		n->backedge = NULL;
-		nodes[i] = n;
-	}
+  // Resize the node list, 2 extra nodes for source and sink.
+  nodes.resize((2 * dice.size()) + 2);
 
-	// Set the indices of the boolean vector to true if the die contains that letter.
-	for (i = 0; i < dice.size(); i++) {
-		for (j = 0; j < dice[i].size(); j++) {
-			if (nodes[i+1]->booleans[dice[i][j]] == 0) {
-				nodes[i+1]->booleans[dice[i][j]] = 1;
-			}
-		}
-	}
+  // Initialize all nodes
+  for (i = 0; i < nodes.size(); i++) {
+    n = new Node;
+    if (i == 0 || i == nodes.size() - 1)
+      n->booleans.resize(256, 1);
+    else
+      n->booleans.resize(256, 0);
+    n->id = i;
+    n->dist = -1;
+    n->backedge = NULL; // No backedge initally.
+    nodes[i] = n;       // Add the nodes to the graph.
+  }
 
-	for (i = 1; i < (1 + dice.size()); i++) {
-		nodes[0]->adj.insert(i);
-	}
-	for (i = 1 + dice.size(); i < nodes.size()-1; i++) {
-		nodes[i]->adj.insert(nodes.size()-1);
-	}
+  // Set the indices of the boolean vector to true if the die contains that
+  // letter.
+  // Iterates through all the dices
+  for (i = 0; i < dice.size(); i++) {
+    // Iterates through the specific dice letters.
+    for (j = 0; j < dice[i].size(); j++) {
+      if (nodes[i + 1]->booleans[dice[i][j]] == 0) {
+        nodes[i + 1]->booleans[dice[i][j]] = 1;
+      }
+    }
+  }
+
+  // Connect source node (index 0) to the dice nodes (index 1 to dice.size())
+  for (i = 1; i < (1 + dice.size()); i++) {
+    // Add an edge from source to each die node
+    nodes[0]->adj.insert(i);
+  }
+  for (i = 1 + dice.size(); i < nodes.size() - 1; i++) {
+    nodes[i]->adj.insert(nodes.size() - 1);
+  }
 }
 
 Graph::~Graph() {
-	int i;
-	for (i = 0; i < nodes.size(); i++) {
-		delete nodes[i];
-	}
-	nodes.clear();
+  // Deletes all nodes and clear the vectors.
+  int i;
+  for (i = 0; i < nodes.size(); i++) {
+    delete nodes[i];
+  }
+  nodes.clear();
 }
 
 void Graph::makegraph(string word) {
-	int i, j;
+  int i, j;
 
-	if (word.size() > dice.size()) {
-		cout << "Cannot spell " << word << endl;
-		return;
-	}
+  // Can't spell word if there are more words than dices.
+  if (word.size() > dice.size()) {
+    cout << "Cannot spell " << word << endl;
+    return;
+  }
 
-	// Adds edges from dice to a letter in the word if the die contains that letter
-	for (i = 1; i < (1 + dice.size()); i++) {
-		for (j = 0; j < word.size(); j++) {
-			if (nodes[i]->booleans[word[j]] == 1) {
-				nodes[i]->adj.insert(j + (1 + dice.size()));
-			}
-		}
-	}
+  // Adds edges from dice to a letter in the word if the die contains that
+  // letter
+  for (i = 1; i < (1 + dice.size()); i++) {
+    for (j = 0; j < word.size(); j++) {
+      if (nodes[i]->booleans[word[j]] == 1) {
+        nodes[i]->adj.insert(j + (1 + dice.size()));
+      }
+    }
+  }
 }
 
+// Resets the graph to its initial state (before processing each word).
 void Graph::reset() {
-	int i;
+  int i;
 
-	for (i = 0; i < nodes.size(); i++) {
-		nodes[i]->adj.clear();
-		nodes[i]->backedge = NULL;
-		nodes[i]->dist = -1;
-	}
+  // Clears all existing nodes
+  for (i = 0; i < nodes.size(); i++) {
+    nodes[i]->adj.clear();
+    nodes[i]->backedge = NULL;
+    nodes[i]->dist = -1;
+  }
 
-	for (i = 1; i < (1 + dice.size()); i++) {
-		nodes[0]->adj.insert(i);
-	}
-	for (i = 1 + dice.size(); i < nodes.size()-1; i++) {
-		nodes[i]->adj.insert(nodes.size()-1);
-	}
+  // Readds the dice nodes
+  // Basically connects the source node to the dice nodes again.
+  for (i = 1; i < (1 + dice.size()); i++) {
+    nodes[0]->adj.insert(i);
+  }
+
+  // Reconnect dice nodes to sink node.
+  for (i = 1 + dice.size(); i < nodes.size() - 1; i++) {
+    nodes[i]->adj.insert(nodes.size() - 1);
+  }
 }
 
+// Implements BFS to find an augmenting path
 void Graph::findpath() {
-	int i;
-	Node *n;
-	set <int>::iterator sit;
+  int i;
+  Node *n;
+  set<int>::iterator sit;
 
-	// Continue until the queue is empty. 
-	while(q.size() != 0) {
-		n = nodes[*(q.begin())];
-		q.pop_front();
-		for(sit = n->adj.begin(); sit != n->adj.end(); sit++) {
-			if (nodes[(*sit)]->dist == -1) {
-				nodes[(*sit)]->dist = 1 + n->dist;
-				nodes[(*sit)]->backedge = n;
-				q.push_back((*sit));
-			}
-		}
-	}
+  // BFS
+  // Continue until the queue is empty.
+  while (q.size() != 0) {
+    n = nodes[*(q.begin())]; // Get the first node in the queue.
+    q.pop_front();           // Pop it
+
+    // Explore the adjacent nodes
+    for (sit = n->adj.begin(); sit != n->adj.end(); sit++) {
+      // Check if the node has been visited.
+      if (nodes[(*sit)]->dist == -1) {
+
+        // If it hasn't been visited then,
+        // Set the distance
+        // Set the backedge for path reconstruction
+        // Then finally add the adjacent node to the queue.
+        nodes[(*sit)]->dist = 1 + n->dist;
+        nodes[(*sit)]->backedge = n;
+        q.push_back((*sit));
+      }
+    }
+  }
 }
 
+// Constructs the augmenting path found by BFS.
 void Graph::makepath() {
-	int j;
-	Node *n;
-	n = nodes[nodes.size()-1];
-	while (n->backedge != NULL) {
-		path.push_back(make_pair(n->id, n->backedge->id));
-		n = n->backedge;
-	}
+  int j;
+  Node *n;
+  n = nodes[nodes.size() - 1]; // Start from the sink node.
+  // Trace back from the sink nodes using the backedges.
+  while (n->backedge != NULL) {
+    // Stores the edge in the path.
+    path.push_back(make_pair(n->id, n->backedge->id));
+    n = n->backedge;
+  }
 }
 
 // Implements the network flow calculations.
-void Graph::adjust() {
-	int i, j;
-	Node *n;
-	set <int>::iterator sit;
+// Updates the graph's flow
 
-	for (i = 0; i < path.size(); i++) {
-		n = nodes[path[i].second];
-		sit = n->adj.find(path[i].first);
-		if (sit != n->adj.end()) {
-			n->adj.erase(sit);
-		}
-		nodes[path[i].first]->adj.insert(path[i].second);
-	}
+// I.e. modify the graph after an augmenting path has been found
+// The purpose of the adjust() function is to get rid of the foward edge
+// and have the reverse edge be the way to tell the
+// iterator, or BFS, to go back and to find a different path.
+void Graph::adjust() {
+  int i, j;
+  Node *n;
+  set<int>::iterator sit;
+
+  // For each edge in the augmenting path
+  for (i = 0; i < path.size(); i++) {
+    n = nodes[path[i].second];
+    sit = n->adj.find(path[i].first);
+    if (sit != n->adj.end()) {
+      n->adj.erase(sit);
+    }
+    nodes[path[i].first]->adj.insert(path[i].second);
+  }
 }
 
 int main(int argc, char *argv[]) {
-	int i, j;						
-	int count;				
-	ifstream file1, file2;	
-	string line;			
-	Graph *g;				
-	vector <string> dice, words;	
-	set <int>::iterator sit;	// Traverses adjacency list
-	count = 0;
+  int i, j;
+  int count;
+  ifstream file1, file2;
+  string line;
+  Graph *g;
+  vector<string> dice, words;
+  set<int>::iterator sit; // Traverses adjacency list
+  count = 0;
 
-	file1.open(argv[1]);
-	while (getline(file1, line)) {
-		dice.push_back(line);
-	}
-	file2.open(argv[2]);
-	while (getline(file2, line)) {
-		words.push_back(line);
-	}
-	file1.close();
-	file2.close();
-	g = new Graph(dice);
+  // Opens the dice.txt file and stores the dices
+  file1.open(argv[1]);
+  while (getline(file1, line)) {
+    dice.push_back(line);
+  }
 
-	// The words vector is traversed and the graph is updated for each word.
-	for (i = 0; i < words.size(); i++) {
-		g->makegraph(words[i]);
-		g->nodes[0]->dist = 0;
-		g->q.push_back(0);
-		g->findpath();
+  // Opens the word.txt file and stores the words.
+  file2.open(argv[2]);
+  while (getline(file2, line)) {
+    words.push_back(line);
+  }
 
-		while(g->nodes[g->nodes.size()-1]->backedge != NULL  && count < words[i].size()) {
-			g->makepath();
-			g->adjust();
-			g->path.clear();
-			for (j = 0; j < g->nodes.size(); j++) {
-				g->nodes[j]->backedge = NULL;
-				g->nodes[j]->dist = -1;
-			}
-			g->nodes[0]->dist = 0;
-			g->q.push_back(0);
-			g->findpath();
-			count++;
-		}
+  file1.close();
+  file2.close();
+  g = new Graph(dice);
 
-		if (count == words[i].size()) {
-			for(j = 0; j < words[i].size(); j++) {
-				sit = g->nodes[j + (1 + dice.size())]->adj.begin();
-				if (j == words[i].size() - 1) {
-					cout << (*sit) - 1 << ": ";
-				}
-				else {
-					cout << (*sit) - 1 << ",";
-				}
-			}
-			cout << words[i] << endl;
-		}
-		else {
-			cout << "Cannot spell " << words[i] << endl;
-		}
+  // The words vector is traversed and the graph is updated for each word.
+  for (i = 0; i < words.size(); i++) {
+    // Build the graph for the current word
+    // Set the source node's distance to 0.
+    // Add the source node to the queue.
+    // Find an augmenting path.
+    g->makegraph(words[i]);
+    g->nodes[0]->dist = 0;
+    g->q.push_back(0);
+    g->findpath();
 
-		/* The graph and count are reset to prep for the next word call. */
-		g->reset();
-		count = 0;
-	}
-	return 0;
+    // For the while loop,
+    // We use BFS to find an augmenting path
+    // Then store the augmenting path
+    // Then we use adjust() to remove the edges used and
+    // add backedges to tell the next BFS iteration that they cannot
+    // use that node again.
+    // Next, we reset the graph, and BFS is run again to find the next
+    // augmenting path. This repeats until the word is fully matched or no more
+    // augmenting paths are found.
+
+    // Try to find enough paths to spell the word.
+    while (g->nodes[g->nodes.size() - 1]->backedge != NULL &&
+           count < words[i].size()) {
+      g->makepath();   // Construct the augmenting path found by BFS.
+      g->adjust();     // Adjust the graph (remove used edges and reverse them).
+      g->path.clear(); // Clear the path vector for the next iteration.
+
+      // Reset all nodes' backedges and distances in preparation for the next
+      // BFS.
+      for (j = 0; j < g->nodes.size(); j++) {
+        g->nodes[j]->backedge = NULL;
+        g->nodes[j]->dist = -1;
+      }
+
+      // Re push the source node and prepare to restart the BFS again.
+      g->nodes[0]->dist = 0;
+      g->q.push_back(0);
+      g->findpath();
+      count++; // Increment the count of letters successfully matched.
+    }
+
+    // After finding all the augmenting paths, check if the word was fully
+    // matched.
+    if (count == words[i].size()) {
+
+      // If the word was successfully spelled, print the result
+      for (j = 0; j < words[i].size(); j++) {
+        // Get the adjacency list of the letter node.
+        sit = g->nodes[j + (1 + dice.size())]->adj.begin();
+        if (j == words[i].size() - 1) {
+          cout << (*sit) - 1 << ": ";
+        } else {
+          cout << (*sit) - 1 << ",";
+        }
+      }
+      cout << words[i] << endl;
+    } else {
+      // If not all letters were matched, print that the word cannot be spelled.
+      cout << "Cannot spell " << words[i] << endl;
+    }
+
+    /* The graph and count are reset to prep for the next word call. */
+    g->reset();
+    count = 0;
+  }
+  return 0;
 }
